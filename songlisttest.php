d920fa044794340f8c5ea01e482ab7733fda8198
root <root@localhost.localdomain>
2015-03-10 23:26:26 -0700

    songlisttest

--- songlisttest.php
+++ songlisttest.php
@@ -10,6 +10,23 @@
 		{
 			$songList=new Songlist("AWord");
 			$this->assertEquals("AWord",$songList->getWord());
+			return $songList;
+		}
+		/**
+		*@depends test_constructor
+		**/
+		function test_lists()
+		{
+
+		}
+		function StringGenerator(){
+			$characters="0123456789abcdefghijklmnopqrstuvwxyz";
+			$cl=strlen($characters);
+			$length=rand(5,10);
+			$randomString='';
+			for($i=0;$i<$length;$i++){
+				$length
+			}
 		}
 	}
 ?>
